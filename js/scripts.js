4cb584dee9ea1e00211bbff0cd3f711b95c93f4a
Aron Eidelman <aron@eideldev.com>
2018-03-30 20:03:45 -0700

    Remove unneeded JS for Yelp

--- js/scripts.js
+++ js/scripts.js
@@ -52,23 +52,6 @@ $(".navbar-nav li a").click(function(event) {
   $(".navbar-collapse").collapse('hide');
 });
 
-// Yelp Review Carousel
-var slideIndex = 1;
-showDivs(slideIndex);
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("yelp-review");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
-
 // Service Tabs
 $('.gridtab-2').gridtab({
   grid: 6,
